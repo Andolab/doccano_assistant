c7502271eab377df1e644b4ea9a36006ada1e522
Ryuya Ikeda <rikeda71@gmail.com>
2019-11-18 20:37:19 +0900

    add discriptions of function and miner fixes

--- doccano2mecab.py
+++ doccano2mecab.py
@@ -1,48 +1,76 @@
-import sys
-import json
-import re
-import glob
+from typing import List, Dict, Any
 import argparse
+import glob
+import json
+import os
+import re
 
-parser = argparse.ArgumentParser(description='Parse doccano export and parsed text to parsed text and labels.')
-parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), help="a file path of doccano export", default=sys.stdin)
-parser.add_argument("infolder", nargs="?", help="a folder path of parsed text", default=sys.stdin)
-parser.add_argument("outfolder", nargs="?", help="a folder path of this program's result", default=sys.stdin)
-args = parser.parse_args()
 
-def return_annotated_text(file_sentence, iob_labels):
+def return_annotated_text(morphs: List[str], iob_labels: List[str]) -> List[str]:
+    """
+
+    Args:
+        morphs (List[str]): morphemes in a sentence
+        iob_labels ([type]): labels formatted iob2 for a sentence
+
+    Returns:
+        List[str]: annotated morphemes
+    """
+
     annotated_text = []
-    for f, j in zip(file_sentence, iob_labels):
-        tmp_text = f.split("\t")
-        tmp_text[-1] = j
-        tmp_text = "\t".join(tmp_text)
+    for morph, label in zip(morphs, iob_labels):
+        tmp_text = morph.split('\t')
+        tmp_text[-1] = label
+        tmp_text = '\t'.join(tmp_text)
         annotated_text.append(tmp_text)
+
     return annotated_text
 
-def return_iob_labels(length, j_labels:list):
+
+def return_iob_labels(lengths: List[int], labels: List[List[Any]]) -> List[str]:
+    """
+
+    Args:
+        lengths (List[int]): list of morph lengths in a morphs
+        labels (List[List[Any]]): list of labels for morphs
+
+    Returns:
+        List[str]: labels for a morphs
+    """
+
     iob_labels = []
     index = 0
     begin_flag = False
-    j_labels.sort(key=lambda x:x[0])
-    for l in length:
-        if len(j_labels) > 0 \
-            and j_labels[0][0] <= index \
-                and j_labels[0][1] > index:
+    labels.sort(key=lambda x: x[0])
+    for length in lengths:
+        if len(labels) > 0 \
+            and labels[0][0] <= index \
+                and labels[0][1] > index:
             if begin_flag:
-                iob_labels.append("I-" + j_labels[0][2])
+                iob_labels.append('I-' + labels[0][2])
             else:
-                iob_labels.append("B-" + j_labels[0][2])
+                iob_labels.append('B-' + labels[0][2])
                 begin_flag = True
         else:
-            iob_labels.append("O")
-        index += l + 1
-        if len(j_labels) > 0 and j_labels[0][1] <= index:
-            j_labels = j_labels[1:]
+            iob_labels.append('O')
+        index += length + 1
+        if len(labels) > 0 and labels[0][1] <= index:
+            labels = labels[1:]
             begin_flag = False
     return iob_labels
 
-def reshape_json(json_file):
-    with open(json_file, 'r', encoding="utf-8") as f:
+
+def reshape_json(json_file: str) -> Dict[str, Any]:
+    """
+
+    Args:
+        json_file (str): json file formatted doccano output
+
+    Returns:
+        Dict[str, Any]: json contents of labeling result by doccano
+    """
+
+    with open(json_file, 'r', encoding='utf-8') as f:
         content = '['
         content += ''.join(
             [re.sub(r'}\n', '},\n', line)
@@ -51,24 +79,43 @@ def reshape_json(json_file):
     json_contents = json.loads(content)
     return json_contents
 
-if __name__ == "__main__":
-    targetfile_path = sys.argv[1]  # doccano
-    old_dir_path = sys.argv[2]  # folder
-    new_dir_path = sys.argv[3]  # new folder
 
-    json_contents = reshape_json(targetfile_path)
-    
-    l = glob.glob("{}/*.txt".format(old_dir_path))
-    json_list = [json_content["text"] for json_content in json_contents]
-    
-    for file_name in l:
-        with open(file_name, "r", encoding="utf-8") as f:
-            parsed_sentence = f.readlines()
-        morphs = [i.split("\t")[0] for i in parsed_sentence]
-        synopsis_sentence = " ".join(morphs)
-        mecab_length = [len(morph) for morph in morphs]
-        i = json_list.index(synopsis_sentence)
-        iob_labels = return_iob_labels(mecab_length, json_contents[i]["labels"])
-        annotated_text = return_annotated_text(parsed_sentence, iob_labels)
-        with open("{}/".format(new_dir_path) + file_name.replace(old_dir_path, ""), "w") as f:
-            f.write("\n".join(annotated_text))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Parse doccano export and parsed \
+            text to parsed text and labels.'
+    )
+    parser.add_argument('infile', nargs=1, type=argparse.FileType('r'),
+                        help='a file path of doccano export')
+    parser.add_argument('indir', nargs=1, type=str,
+                        help='a directory path of parsed text')
+    parser.add_argument('outdir', nargs=1, type=str,
+                        help='a directory path of this program\'s result')
+    args = parser.parse_args()
+    os.makedirs(args.outdir, exist_ok=True)
+
+    json_contents = reshape_json(args.infile)
+
+    files = glob.glob('{}/*.txt'.format(args.indir))
+    json_list = [json_content['text'] for json_content in json_contents]
+
+    for fname in files:
+        with open(fname, 'r', encoding='utf-8') as f:
+            parsed_morphs = f.readlines()
+        morphs = [s.split('\t')[0] for s in parsed_morphs]
+        synopsis_morphs = ' '.join(morphs)
+
+        # make an annotated string
+        morph_lengths = [len(morph) for morph in morphs]
+        idx = json_list.index(synopsis_morphs)
+        iob_labels = return_iob_labels(
+            morph_lengths, json_contents[idx]['labels']
+        )
+        annotated_text = return_annotated_text(parsed_morphs, iob_labels)
+
+        # create an annotated text
+        target_file = '{}/{}'.format(
+            args.outdir, fname.replace(args.indir, '')
+        )
+        with open(target_file, 'w') as f:
+            f.write('\n'.join(annotated_text))
--- mecab2doccano.py
+++ mecab2doccano.py
@@ -1,14 +1,18 @@
-import sys
+import argparse
 import glob
 import re
 
 
-def mecab2doccano(path, partition):
+def mecab2doccano(path: str, partition: str) -> str:
     """
     texts of mecab format to texts of doccano format
-    :param path : path of a file containing morphologically parsed text in mecab
-    :param partition : the simbol of morph partition
-    :return texts converted doccano format
+
+    Args:
+        path (str): path of a file containing morphologically parsed text in mecab
+        partition (str): the simbol of morph partition
+
+    Returns:
+        str: converted doccano format
     """
 
     with open(path, 'r') as f:
@@ -19,13 +23,24 @@ def mecab2doccano(path, partition):
         morphs = [morph for morph in morphs if len(morph.split(partition)) > 1]
         words += ' '.join([morph.split(partition)[0] for morph in morphs]) + ' ' \
             if len(morphs) > 0 else ''
-    words = re.sub('\s+$', '', words)
+    words = re.sub(r'\s+$', '', words)
     return words
 
 
 if __name__ == '__main__':
-    mecab_format_dir = sys.argv[1]
-    parsed_text_path = sys.argv[2]
-    converted_text = '\n'.join(mecab2doccano(path, '\t') for path in glob.glob(mecab_format_dir + '*'))
-    with open(parsed_text_path, 'w') as f:
+    parser = argparse.ArgumentParser(
+        description='Parse texts formatted mecab -> texts formatted doccano input'
+    )
+    parser.add_argument('indir', nargs=1, type=str,
+                        help='a directory path containing parsed texts by mecab')
+    parser.add_argument('outpath', nargs=1, type=str,
+                        help='a output file path')
+    args = parser.parse_args()
+    mecab_format_dir = args.indir + '/' \
+        if args.indir[-1] != '/' else args.indir
+    converted_text = '\n'.join(
+        mecab2doccano(path, '\t')
+        for path in glob.glob(mecab_format_dir + '*')
+    )
+    with open(args.outpath, 'w') as f:
         f.write(converted_text)
